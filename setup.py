df181a9c8347bb24cc8a2c2361640c415a632173
Jared Hobbs <jared@pyhacker.com>
2018-03-21 15:26:30 -0700

    version bump for pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 5):
 
 
 setup(name='postgres_audit_triggers',
-      version='0.1.0',
+      version='0.1.1',
       author='Jared Hobbs',
       author_email='jared.hobbs@carta.com',
       license='MIT',
